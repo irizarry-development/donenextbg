6f545afe33bf7ff9e8461e52ba5b038c8d227de4
Matthew Irizarry <matthewjirizarry@gmail.com>
2024-03-19 16:26:37 -0500

    Implemented the coverage page.

--- app/coverage/page.tsx
+++ app/coverage/page.tsx
@@ -5,40 +5,57 @@ import { Circle } from "~/components/map/Circle";
 
 export default function CoveragePage() {
 
-const DEFAULT_MAP_CENTER = {
-    lat: 36.9636545,
-    lng: -86.4805194
+    const DEFAULT_MAP_CENTER = {
+        lat: 36.9636545,
+        lng: -86.4805194
     }
 
     return (
 
-        <section className="coverage-details">
-        <h1>Our Coverage Area</h1>
-        <p>Check out our coverage area to see if we service your area.</p>
+        <section className="coverage-page">
 
-        <section className="coverage-map">
-            <APIProvider
-            apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}
-            >
-                <Map
-                    style={{width: '100vw', height: '100vh'}}
-                    defaultCenter={DEFAULT_MAP_CENTER}
-                    defaultZoom={8}
-                    gestureHandling={'greedy'}
-                    disableDefaultUI={true}
+            <section className="coverage-map">
+                <APIProvider
+                    apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}
                 >
-                    <Circle
-                radius={120000}
-                center={DEFAULT_MAP_CENTER}
-                strokeColor={'#0c4cb3'}
-                strokeOpacity={1}
-                strokeWeight={3}
-                fillColor={'#3b82f6'}
-                fillOpacity={0.3}
-                />
-                </Map>
-            </APIProvider>
+                    <Map
+                        style={{ width: '100vw', height: '100vw' }}
+                        defaultCenter={DEFAULT_MAP_CENTER}
+                        defaultZoom={7}
+                        gestureHandling={'cooperative'}
+                        disableDefaultUI={true}
+                    >
+                        <Circle
+                            radius={120000}
+                            center={DEFAULT_MAP_CENTER}
+                            strokeColor={'#0c4cb3'}
+                            strokeOpacity={1}
+                            strokeWeight={3}
+                            fillColor={'#3b82f6'}
+                            fillOpacity={0.3}
+                        />
+                    </Map>
+                </APIProvider>
+            </section>
+            <section className="coverage-details">
+                <h1>Our Coverage Area</h1>
+                <p>
+                    We are proud to serve the following areas:
+                </p>
+                <ul>
+                    <li>Bowling Green</li>
+                    <li>Glasgow</li>
+                    <li>Scottsville</li>
+                    <li>Franklin</li>
+                    <li>Portland</li>
+                    <li>Lebanon</li>
+                    <li>Springfield</li>
+                    <li>Elizabethtown</li>
+                    <li>Greensburg</li>
+                    <li>Edmonton</li>
+                    <li>And more!</li>
+                </ul>
+            </section>
         </section>
-    </section>
     )
 }
