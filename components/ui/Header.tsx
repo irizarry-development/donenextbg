ef3da12538773063b810b8d6ff6d3a61cd5dd6d6
Matthew Irizarry <matthewjirizarry@gmail.com>
2024-03-19 15:30:57 -0500

    Basic Navigation Bar is working.

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { FaBars } from "react-icons/fa6";
+import { FaBars, FaCircleInfo, FaHouse, FaPhone } from "react-icons/fa6";
 
 export default function Header() {
 
@@ -12,10 +12,12 @@ export default function Header() {
     return (
         <header className="app-header">
           <section className="app-header-logo" />
-          <nav className="app-navigation">
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
+          <nav className={`app-navigation ${isMenuOpen && 'open'}`}>
+            <section className="app-navigation-content">
+              <a className="app-navigation-link" href="/">Home <FaHouse /></a>
+              <a className="app-navigation-link" href="/about">About <FaCircleInfo /></a>
+              <a className="app-navigation-link" href="/contact">Contact <FaPhone /></a>
+            </section>
           </nav>
           <FaBars 
             className="app-menu-toggle"
